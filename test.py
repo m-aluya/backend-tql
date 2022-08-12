e3f8670c9d945a973c8c99690af8eb202f7b42d4
m-aluya <maluya4@gmail.com>
2022-08-12 14:39:24 +0100

    change base URL

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 import requests
 from concurrent.futures import ThreadPoolExecutor
-BASE = "https://discretematt.herokuapp.com"
+BASE = "https://orca-app-3nf25.ondigitalocean.app/"
 def get_url(url):
     return requests.get(url)
 with ThreadPoolExecutor(max_workers=50) as pool:
